eed1676bed41d36ed1e232ee695069c2e7f83e46
ashiksaibabu <ashik.dockode@gmail.com>
2025-10-20 23:59:03 +0530

    Update UI

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,33 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [pingResponse, setPingResponse] = useState('');
+  const [pingResponse, setPingResponse] = useState("");
   const backendPort = process.env.REACT_APP_BACKEND_PORT || 5001;
 
   const handlePingClick = () => {
     fetch(`http://localhost:${backendPort}/ping`)
-      .then(response => response.json())
-      .then(data => setPingResponse(data.message))
-      .catch(error => console.error('Error:', error));
+      .then((response) => response.json())
+      .then((data) => setPingResponse(data.message))
+      .catch((error) => console.error("Error:", error));
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={handlePingClick}>Ping</button>
-        {pingResponse && <div style={{ marginLeft: '10px' }}>{pingResponse}</div>}
-      </header>
+      <div className="card w-96 bg-base-100 card-xs shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Ping Reponse</h2>
+          <p>{pingResponse ? pingResponse : "No response yet"}</p>
+          <div className="justify-end card-actions">
+            <button
+              onClick={handlePingClick}
+              className="btn btn-dash btn-success"
+            >
+              Ping
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
